14b400a9a97f9dc196448f89059b840808cc4c0e
jonian <jonian@hardpixel.eu>
2022-09-29 15:52:00 +0300

    optimize build script

--- build.sh
+++ build.sh
@@ -5,24 +5,23 @@ set -e
 STACK_VERSIONS=(18 20 22)
 
 for stack_version in "${STACK_VERSIONS[@]}"; do
-  image_name=libvips-heroku-$stack_version:$VIPS_VERSION
+  image_name=libvips/heroku-$stack_version:$VIPS_VERSION
 
   docker build \
+    --file container/Dockerfile \
     --build-arg VIPS_VERSION=${VIPS_VERSION} \
     --build-arg STACK_VERSION=${stack_version} \
-    -t $image_name \
-    container
+    --tag $image_name \
+    build
 
   mkdir -p build
+  docker cp $(docker create $image_name):/usr/local/vips/build/. build
 
-  docker run --rm -t -v $PWD/build:/build --user $(id -u):$(id -g) \
-    $image_name sh -c "cp -rf /usr/local/vips/build/* /build"
-
-  image_name=libvips-heroku-test-$stack_version:$VIPS_VERSION
+  image_name=libvips/heroku-test-$stack_version:$VIPS_VERSION
 
   docker build \
     --file container/Dockerfile.test \
     --build-arg STACK_VERSION=${stack_version} \
-    -t $image_name \
+    --tag $image_name \
     build
 done
